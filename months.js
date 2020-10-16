bee03c9d4bfe94deefd826a6bd62454627d11b09
LunaticHacker <lunatechnology007@gmail.com>
2020-10-16 10:05:04 -0500

    initial work

---
+++ months.js
@@ -0,0 +1,15 @@
+//Long Term Goal : Expand this idea and  associate each day with an emoji;
+let m_data = {
+  0: { emojis: ["🎆"], color: "blue" },
+  1: { emojis: ["💖"], color: "red" },
+  2: { emojis: ["👩"], color: "white" },
+  3: { emojis: ["🐀", "🐁"], color: "white" },
+  4: { emojis: ["👩‍👦", "👩‍👧"], color: "white" },
+  5: { emojis: ["🏳️‍🌈", "🌈"], color: "purple" },
+  6: { emojis: ["🤣", "😂"], color: "white" },
+  7: { emojis: ["🐈"], color: "white" },
+  8: { emojis: ["☮️"], color: "white" },
+  9: { emojis: ["🎃"], color: "orange" },
+  10: { emojis: ["🥗"], color: "green" },
+  11: { emojis: ["☃️", "❄️"], color: "red" },
+};
---
+++ script.js
@@ -0,0 +1,34 @@
+const words = ["Sometimes Poems,", "Mostly Code !!"];
+let wordIndex = 0;
+let charIndex = 0;
+(function boom() {
+  if (wordIndex == words.length) wordIndex = 0;
+  let currentText = words[wordIndex].slice(0, ++charIndex);
+  document.querySelector(".typing").textContent = currentText;
+  if (currentText.length == words[wordIndex].length) {
+    wordIndex++;
+    charIndex = 0;
+  }
+  setTimeout(boom, 500);
+})();
+(function drop() {
+  let month = new Date().getMonth();
+  for (let i = 0; i < 30; i++) {
+    let emoji = document.createElement("div");
+    emoji.classList.add("emoji");
+    emoji.style.left = (Math.random() * window.innerWidth) / 4 + "px";
+    emoji.style.top = (Math.random() * window.innerHeight) / 2 + "px";
+    emoji.textContent = `${randomFromArray(m_data[month].emojis)}`;
+    document.body.appendChild(emoji);
+    document.body.style.color = m_data[month].color;
+    let links = document.querySelectorAll("a");
+    for (link of links) link.style["border-color"] = m_data[month].color;
+  }
+})();
+
+function randomFromArray(arr) {
+  let len = arr.length;
+  let r = Math.floor(Math.random() * len);
+  console.log(r);
+  return arr[r];
+}
